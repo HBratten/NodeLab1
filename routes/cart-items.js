cd6af407b17a7a90ef15cfa0dad184e5511f5210
Hillary <uptonh13@gmail.com>
2019-08-23 13:56:42 -0400

    Set up server, routes, and cart-items

---
+++ routes/cart-items.js
@@ -0,0 +1,8 @@
+const cart = [
+  { id: "1", product: "banana", price: ".19", quantity: "4" },
+  { id: "2", product: "watermelon", price: "4.99", quantity: "1" },
+  { id: "3", product: "cucumber", price: ".99", quantity: "2" },
+  { id: "4", product: "red pepper", price: ".49", quantity: "3" }
+];
+
+module.exports = cart;
