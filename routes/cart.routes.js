66c72239450bb4261fcd72c2edf3ebc16cb5e59f
Hillary <uptonh13@gmail.com>
2019-08-28 22:25:01 -0400

    Backend

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,29 +1,44 @@
 const express = require("express");
 const cartRoutes = express.Router();
 const cart = require("./cart-items");
+const pool = require("../connection/pg-connection-pool");
 
-cartRoutes.get("/cart-items", (req, res) => {
-  res.send(cart);
+function selectAllShoppingCart(req, res) {
+  pool.query("select * from shoppingcart order by id").then(result => {
+    res.send(result.rows);
+  });
+}
+
+cartRoutes.get("/shoppingcart", selectAllShoppingCart);
+
+cartRoutes.post("/shoppingcart", (req, res) => {
+  pool
+    .query(
+      "insert into shoppingcart(product, price, quantity) values ($1::text, $2::numeric, $3::int)",
+      [req.body.product, req.body.price, req.body.quantity]
+    )
+    .then(() => {
+      selectAllShoppingCart(req, res);
+    });
 });
 
-cartRoutes.post("/cart-items", (req, res) => {
-  console.log(req.body);
-  // cart.push(req.body);
-  // res.send(cart);
+cartRoutes.put("/shoppingcart/:id", (req, res) => {
+  pool
+    .query("update shoppingcart set quantity=$1::int where id=$2::int", [
+      req.body.quantity,
+      req.params.id
+    ])
+    .then(() => {
+      selectAllShoppingCart(req, res);
+    });
 });
 
-cartRoutes.put("/cart-items/:id", (req, res) => {
-  console.log(req.body, req.params.id);
-  // const index = cart.findIndex(item => item.id === req.params.id);
-  // cart.splice(index, 1, req.body);
-  // res.send(cart);
-});
-
-cartRoutes.delete("/cart-items/:id", (req, res) => {
-  console.log(req.params.id);
-  // const index = cart.findIndex(item => item.id === req.params.id);
-  // cart.splice(index, 1);
-  // res.send(cart);
+cartRoutes.delete("/shoppingcart/:id", (req, res) => {
+  pool
+    .query("delete from shoppingcart where id=$1::int", [req.params.id])
+    .then(() => {
+      selectAllShoppingCart(req, res);
+    });
 });
 
 module.exports = cartRoutes;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5100;
+const port = 5010;
 const cartRoutes = require("./routes/cart.routes");
 const cors = require("cors");
 
